2449ff021c5f31c7bbb92771e6aea1b0d2731430
Ben Hearsum <ben@hearsum.ca>
2021-05-23 21:59:26 -0400

    Rework locations into an array with memory addresses and names

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ mod locations;
 
 fn main() {
     let mut alttpr = File::open("alttpr.sfc").expect("Failed to open rom");
-    alttpr.seek(std::io::SeekFrom::Start(locations::LINKS_HOUSE.rom_addr)).expect("Couldn't seek");
 
-    let mut item = [0; 1];
-    alttpr.read(&mut item).expect("Couldn't read");
-    println!("Link's House contains: {:?}", item);
+    for loc in &locations::LOCATIONS {
+        alttpr.seek(std::io::SeekFrom::Start(loc.rom_addr)).expect("Couldn't seek");
+        let mut item = [0; 1];
+        alttpr.read(&mut item).expect("Couldn't read");
+        println!("{}'s contains: {:?}", loc.name, item);
+    }
 }
