7fe3ba7fbc9360c8014daaf633b255c94721c661
Ben Hearsum <ben@hearsum.ca>
2021-05-24 10:55:55 -0400

    Formatting pass

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
-use std::io::Seek;
 use std::io::prelude::*;
+use std::io::Seek;
 
 mod items;
 mod locations;
@@ -9,9 +9,15 @@ fn main() {
     let mut alttpr = File::open("alttpr.sfc").expect("Failed to open rom");
 
     for loc in &locations::LOCATIONS {
-        alttpr.seek(std::io::SeekFrom::Start(loc.rom_addr)).expect("Couldn't seek");
+        alttpr
+            .seek(std::io::SeekFrom::Start(loc.rom_addr))
+            .expect("Couldn't seek");
         let mut item_value = [0; 1];
         alttpr.read(&mut item_value).expect("Couldn't read");
-        println!("{}'s contains: {}", loc.name, items::get_item(item_value[0]).name);
+        println!(
+            "{}'s contains: {}",
+            loc.name,
+            items::get_item(item_value[0]).name
+        );
     }
 }
